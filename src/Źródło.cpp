c26800fdea11227105bb696f10c26e7f0c501232
MiniKarlito <wiktorlinek10@gmail.com>
2026-01-04 16:46:09 +0100

    tla, punkty za trafienie prezentem do komina

--- src/Źródło.cpp
+++ src/Źródło.cpp
@@ -3,7 +3,6 @@
 #include <windows.h>
 #include <vector>
 #include <algorithm>
-
 struct m
 {
     float szerokosc;
@@ -15,26 +14,39 @@ struct ostrzerzenie
     sf::Clock Timer;
     bool wystrzelono = false;
 };
+struct zakres
+{
+    float start;
+    float koniec;
+};
 
 //do wektorow(!!!dla kciuka!!!) - kazdy indeks to nowy poziom, jak chcesz zmienic predkosc jakiegos obiektu to tutaj
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-std::vector<float> predkosci_mikolaj = { 200.0f,200.0f,200.0f,200.0f,200.0f,200.0f};
-std::vector<float> predkosci_prezent_x = { 0.0f,0.0f,0.0f,0.0f,0.0f,0.0f};
-std::vector<float> predkosci_prezent_y = { 200.0f,200.0f,200.0f,200.0f,200.0f,200.0f};
-std::vector<float> predkosci_domek = { 100.0f,100.0f,100.0f,100.0f,100.0f,100.0f};
-std::vector<float> predkosci_fajerwerki_x = { 400.0f,400.0f,400.0f,400.0f,400.0f,400.0f};
-
+std::vector<float> predkosci_mikolaj = { 200.0f,200.0f,200.0f,200.0f,200.0f,200.0f };
+std::vector<float> predkosci_prezent_x = { 0.0f,0.0f,0.0f,0.0f,0.0f,0.0f };
+std::vector<float> predkosci_prezent_y = { 200.0f,200.0f,200.0f,200.0f,200.0f,200.0f };
+std::vector<float> predkosci_domek = { 100.0f,100.0f,100.0f,100.0f,100.0f,100.0f };
+std::vector<float> predkosci_fajerwerki_x = { 400.0f,400.0f,400.0f,400.0f,400.0f,400.0f };
+std::vector<zakres> zakresy_kominow =
+{
+    {0.20f, 0.33f}, // dom1
+    {0.70f, 0.85f}, // dom2
+    {0.75f, 0.91f}, // dom3
+    {0.34f, 0.51f}, // dom4
+    {0.76f, 0.94f}  // dom5
+};
+int wysokosc_hitboxa = 10.0f;
 void setSpeed(int poziom, float& predkosc_mikolaj_y, float& predkosc_prezent_x, float& predkosc_prezent_y, float& predkosc_domek_x, float& predkosc_fajerwerek_x)
 {
     int index = poziom - 1;
-    if(index<=5)
+    if (index <= 5)
     {
-    predkosc_mikolaj_y = predkosci_mikolaj[index];
-    predkosc_prezent_x = predkosci_prezent_x[index];
-    predkosc_prezent_y = predkosci_prezent_y[index];
-    predkosc_domek_x = predkosci_domek[index];
-    predkosc_fajerwerek_x = predkosci_fajerwerki_x[index];
+        predkosc_mikolaj_y = predkosci_mikolaj[index];
+        predkosc_prezent_x = predkosci_prezent_x[index];
+        predkosc_prezent_y = predkosci_prezent_y[index];
+        predkosc_domek_x = predkosci_domek[index];
+        predkosc_fajerwerek_x = predkosci_fajerwerki_x[index];
     }
     else
     {
@@ -48,14 +60,39 @@ int main() {
     int szerokosc_okna = 1200;
     int wysokosc_okna = 900;
     sf::RenderWindow window(sf::VideoMode(szerokosc_okna, wysokosc_okna), "SantaGame");
-
     //kurwa potrzebuje poziomow XDD
     //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     int poziom = 1;
+    int punkty = 0;
+    //tla i skala
+    std::vector<sf::Texture> tla;
+    sf::Texture grafika;
+    grafika.loadFromFile("Europa_k.png");
+    tla.push_back(grafika);
+    grafika.loadFromFile("Afryka.png");
+    tla.push_back(grafika);
+    grafika.loadFromFile("Ameryka.png");
+    tla.push_back(grafika);
+    grafika.loadFromFile("Azja.png");
+    tla.push_back(grafika);
+    grafika.loadFromFile("Biegun.png");
+    tla.push_back(grafika);
+
+    sf::Sprite tlo1, tlo2;
+    tlo1.setTexture(tla[poziom - 1]);
+    sf::Vector2u tloSize = tla[poziom - 1].getSize();
+    tlo1.setScale((float)szerokosc_okna / tloSize.x, (float)wysokosc_okna / tloSize.y);
+    tlo1.setPosition(0.0f, 0.0f);
+    tlo2.setTexture(tla[poziom - 1]);
+    tlo2.setScale((float)szerokosc_okna / tloSize.x, (float)wysokosc_okna / tloSize.y);
+    float szerokosc_tla = tlo1.getGlobalBounds().width;
+    tlo2.setPosition(szerokosc_tla, 0.0f);
+    float tlo1_x = 0.0f, tlo2_x = szerokosc_tla;
+
     setSpeed(poziom, predkosc_mikolaj_y, predkosc_prezent_x, predkosc_prezent_y, predkosc_domek_x, predkosc_fajerwerek_x);
     //tekstura mikolaja
     sf::Texture tekstura_mikolaj;
-    if (!tekstura_mikolaj.loadFromFile("mikolaj.png"))
+    if (!tekstura_mikolaj.loadFromFile("mikolajp1.png"))
     {
         char buffer[MAX_PATH];
         GetCurrentDirectoryA(MAX_PATH, buffer);
@@ -69,6 +106,14 @@ int main() {
         std::cout << "blad ladowania czcionki" << std::endl;
         return 1;
     }
+    //punkty
+    sf::Text tekst_punktow;
+    tekst_punktow.setFont(czcionka_arial);
+    tekst_punktow.setCharacterSize(30);       // rozmiar czcionki
+    tekst_punktow.setFillColor(sf::Color::Yellow); // kolor tekstu
+    tekst_punktow.setPosition(20.0f, 20.0f);  // pozycja w lewym górnym rogu
+    tekst_punktow.setStyle(sf::Text::Bold);
+
     //napis przy lataniu za nisko
     sf::Text napis_lot;
     napis_lot.setFont(czcionka_arial);
@@ -106,14 +151,18 @@ int main() {
 
     //tekstury domkow
     std::vector<sf::Texture> tekstury_domkow;
-    sf::Texture t1, t2, t3;
-    t1.loadFromFile("domek1.png");
-    t2.loadFromFile("domek2.png");
-    t3.loadFromFile("domek3.png");
-
+    sf::Texture t1;
+    t1.loadFromFile("dom1.png");
     tekstury_domkow.push_back(t1);
-    tekstury_domkow.push_back(t2);
-    tekstury_domkow.push_back(t3);
+    t1.loadFromFile("dom2.png");
+    tekstury_domkow.push_back(t1);
+    t1.loadFromFile("dom3.png");
+    tekstury_domkow.push_back(t1);
+    t1.loadFromFile("dom4.png");
+    tekstury_domkow.push_back(t1);
+    t1.loadFromFile("dom5.png");
+    tekstury_domkow.push_back(t1);
+
 
     //tekstury fajerwerek
     std::vector<sf::Texture> tekstury_fajerwerek;
@@ -146,7 +195,7 @@ int main() {
     float cooldown_resp_fajerwerek = 5.0f;
 
     // mikolaj
-    m k1 = { 400.0f, 120.0f };
+    m k1 = { 300.0f, 120.0f };
     sf::Sprite mikolaj;
     mikolaj.setTexture(tekstura_mikolaj);
     sf::Vector2u texSize = tekstura_mikolaj.getSize();
@@ -159,15 +208,18 @@ int main() {
     // wiele prezentów
     std::vector<sf::Sprite> prezenty;
 
-
     //wiele domkow
     std::vector<sf::Sprite> domki;
+
+    //hitboxy domkow
+    std::vector<sf::RectangleShape> hitboxy;
 
     //wiele fajerwerek 
     std::vector<sf::Sprite> fajerwerki;
 
     //wykrzykniki
     std::vector<ostrzerzenie> ostrzerzenia;
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -177,14 +229,26 @@ int main() {
         }
 
         float dt = clock.restart().asSeconds();
-        ////////////////
+        //cala mechnika tla(ruch i zapetlanie)
 
+        float predkosc_tla = 200.0f;//stala wartosc(tego nie zmieniamy w zaleznosci od poziomow)
+
+        tlo1_x -= predkosc_tla * dt;
+        tlo2_x -= predkosc_tla * dt;
+        if (tlo1_x + szerokosc_tla <= 0.0f)
+        {
+            tlo1_x = tlo2_x + szerokosc_tla - 5.0f;
+        }
+        if (tlo2_x + szerokosc_tla <= 0.0f)
+        {
+            tlo2_x = tlo1_x + szerokosc_tla - 5.0f;
+        }
+        tlo1.setPosition(tlo1_x, 0.0f);
+        tlo2.setPosition(tlo2_x, 0.0f);
         // ruch mikolaja
         sf::Vector2f pos = mikolaj.getPosition();
         sf::FloatRect bounds = mikolaj.getGlobalBounds();
         sf::Vector2f newPos = pos;
-
-        //predkosc_mikolaj_y = 200.0f;
 
         if (window.hasFocus())
         {
@@ -215,7 +279,6 @@ int main() {
 
         mikolaj.setPosition(newPos);
 
-
         // tworzenie nowego prezentu + cooldown
 
         if (window.hasFocus())
@@ -240,8 +303,6 @@ int main() {
         }
 
         // aktualizacja prezentow
-        //predkosc_prezent_x = 0.0f;
-        //predkosc_prezent_y = 200.0f;
         for (auto& p : prezenty)
         {
             sf::Vector2f pos_p = p.getPosition();
@@ -250,45 +311,70 @@ int main() {
             p.setPosition(pos_p);
         }
 
-
         // usuwanie prezentow na dole okna
-
         prezenty.erase(
             std::remove_if(prezenty.begin(), prezenty.end(),
                 [&](sf::Sprite& p) {
-                    return p.getPosition().y + p.getGlobalBounds().height >= wysokosc_okna;
+                    if (p.getPosition().y + p.getGlobalBounds().height >= wysokosc_okna)
+                    {
+                        return true;
+                    }
+                    for (auto& h : hitboxy)
+                    {
+                        if (p.getGlobalBounds().intersects(h.getGlobalBounds()))
+                        {
+                            punkty += 1;
+                            return true;
+                        }
+                    }
+                    return false;
                 }),
             prezenty.end()
         );
 
-
-        //tworzenie domku + cooldown
+        //tworzenie domku + cooldown + hitbox komina
         if (cooldown_domku.getElapsedTime().asSeconds() >= cooldown_resp_domku)
         {
             sf::Sprite domek;
-            int index = rand() % 3;
-            domek.setTexture(tekstury_domkow[index]);//losowa grafika z vectora(indeksy 0-2)
+            int index = rand() % 5;
+            domek.setTexture(tekstury_domkow[index]);
 
             //skalowanie
             sf::Vector2u domek_size = tekstury_domkow[index].getSize();
-            scaleX = 220.0f / domek_size.x;
-            scaleY = 220.0f / domek_size.y;
+            scaleX = 250.0f / domek_size.x;
+            scaleY = 250.0f / domek_size.y;
             domek.setScale(scaleX, scaleY);
 
             domek.setPosition(szerokosc_okna, wysokosc_okna - 220.0f);
 
             domki.push_back(domek);
+
+            sf::RectangleShape hitbox;
+            float hitbox_start = zakresy_kominow[index % 5].start * (domek_size.x * scaleX);
+            float hitbox_end = zakresy_kominow[index % 5].koniec * (domek_size.x * scaleX);
+
+            hitbox.setSize(sf::Vector2f(hitbox_end - hitbox_start, wysokosc_hitboxa));
+            hitbox.setPosition(domek.getPosition().x + hitbox_start, domek.getPosition().y-5.0f);
+            hitbox.setFillColor(sf::Color::Black);
+
+            hitboxy.push_back(hitbox);
+
             cooldown_domku.restart();
             cooldown_resp_domku = 3.0f + 3.0f * (rand() / (float)RAND_MAX);
         }
 
         //aktualizacja wszystkich domkow
-        //predkosc_domek_x = 100.0f;
         for (auto& d : domki)
         {
             sf::Vector2f pos_d = d.getPosition();
             pos_d.x -= predkosc_domek_x * dt;
             d.setPosition(pos_d);
+        }
+        for (auto& h : hitboxy)
+        {
+            sf::Vector2f pos_h = h.getPosition();
+            pos_h.x -= predkosc_domek_x * dt;
+            h.setPosition(pos_h);
         }
 
         domki.erase(
@@ -298,8 +384,13 @@ int main() {
                 }),
             domki.end()
         );
-
-
+        hitboxy.erase(
+            std::remove_if(hitboxy.begin(), hitboxy.end(),
+                [&](sf::RectangleShape& h) {
+                    return h.getPosition().x + h.getSize().x < 0;
+                }),
+            hitboxy.end()
+        );
 
         //tworzenie wykrzyknikow do fajerwerek + cooldown
         if (cooldown_fajerwerek.getElapsedTime().asSeconds() >= cooldown_resp_fajerwerek)
@@ -314,28 +405,26 @@ int main() {
 
                 //skalowanie wykrzyknika
                 sf::Vector2u wykrzyknik_size = tekstura_wykrzyknik.getSize();
-                scaleX = 30.0f / wykrzyknik_size.x;
-                scaleY = 30.0f / wykrzyknik_size.y;
+                scaleX = 50.0f / wykrzyknik_size.x;
+                scaleY = 50.0f / wykrzyknik_size.y;
                 o.wykrzyknik.setScale(scaleX, scaleY);
                 o.Timer.restart();
 
                 ostrzerzenia.push_back(o);
 
-
-
-
             }
             cooldown_fajerwerek.restart();
-            cooldown_resp_fajerwerek = 10.0f + 5.0f * (rand() / (float)RAND_MAX); //losowy czas między 10-15s
+            cooldown_resp_fajerwerek = 10.0f + 5.0f * (rand() / (float)RAND_MAX);
         }
+
         //fajerwerki
         for (auto& o : ostrzerzenia)
         {
-            if (o.Timer.getElapsedTime().asSeconds() >= 1.0f && !o.wystrzelono) // sekunde po pokazaniu leca fajerwerki
+            if (o.Timer.getElapsedTime().asSeconds() >= 1.0f && !o.wystrzelono)
             {
                 sf::Sprite fajerwerka;
                 int index = rand() % 3;
-                fajerwerka.setTexture(tekstury_fajerwerek[index]);//losowa grafika z vectora(indeksy 0-2)
+                fajerwerka.setTexture(tekstury_fajerwerek[index]);
 
                 //skalowanie
                 sf::Vector2u fajerwerka_size = tekstury_fajerwerek[index].getSize();
@@ -357,7 +446,6 @@ int main() {
         );
 
         //aktualizacja wszystkich fajerwerek
-        //predkosc_fajerwerek_x = 400.0f;
         for (auto& f : fajerwerki)
         {
             sf::Vector2f pos_f = f.getPosition();
@@ -380,14 +468,16 @@ int main() {
                 window.close();
         }
         window.clear(sf::Color::Black);
+        window.draw(tlo1);
+        window.draw(tlo2);
+
         //wyswietlanie napisu podczas kolizji
         for (auto& f : fajerwerki) {
             if (mikolaj.getGlobalBounds().intersects(f.getGlobalBounds())) {
                 window.draw(napis_kolizja);
-                break; // nie ma sensu sprawdzać dalej 
+                break;
             }
         }
-
 
         // rysowanie wszystkich prezentów
         for (auto& p : prezenty)
@@ -402,10 +492,13 @@ int main() {
         // rysowanie wszystkich fajerwerek
         for (auto& f : fajerwerki)
             window.draw(f);
+        //for (auto& h : hitboxy)
+        //  window.draw(h);
         if (newPos.y + bounds.height == wysokosc_okna * 0.6f)
             window.draw(napis_lot);
         window.draw(mikolaj);
+        tekst_punktow.setString(std::to_string(punkty));
+        window.draw(tekst_punktow);
         window.display();
     }
 }
-
